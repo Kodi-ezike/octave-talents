d5548e4ce828e8b527f4db6f32dff22054a730a3
Muhammad <muhdibee@gmail.com>
2022-04-17 15:57:45 +0100

    Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import Header from './Components/Header';
 import Login from './Components/Login';
+import Home from './Components/Home';
+
 function App() {
 
   return (
     <div className="App">
       <Header/>
-      <Login/>
+      {/* <Login/> */}
+      <Home/>
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+import './home.css';
+
+const Home = () => {
+    return (
+        <div class="homewrapper">
+            <div className='row1'>
+                <div className='col1'>
+                    <h1>Outsourcing the best tech talents</h1>
+                    <p>We provide the best technology talents to build solutions that best suit your needs.</p>
+                </div>
+                <div className='col2'>
+                    <div className="image1"> Image</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
