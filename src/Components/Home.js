88b8423a9d916e6a163839d5dffb4e855b548f7a
Muhammad <muhdibee@gmail.com>
2022-04-20 10:04:33 +0100

    Added and Styled row2 background-image

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,19 +4,19 @@ import Rectangle9 from './images/Rectangle9.svg';
 const Home = () => {
     return (
         <div class="homewrapper">
-            <div className='row1'>
+            <div className='row row1'>
                 <div className='col1'>
                     <h1>Outsourcing the best tech talents</h1>
                     <p>We provide the best technology talents to build solutions that best suit your needs.</p>
                 </div>
-                <div className='col2'>
-                    <div className="image1"> Image</div>
+                <div className='col2'></div> {/* Image is palced as a backgroundImage in css */}
+            </div>
+            <div className='row row2Background'>{/* Image is palced as a backgroundImage  in css */}
+                <div className='row row2'>
+                    <h2>For clients</h2>
                 </div>
             </div>
-            <div className='row2' style={{backgroundImage: `url(${Rectangle9})`, backgroundRepeat:'no-repeat', backgroundSize: "cover"}}>
-                <p>sadvasd</p>
 
-            </div>
         </div>
     )
 }
